2c4a4f78b8e6f79acc3e53b6b3103fe5b3cab26b
Kriti Kumari <kriti.cs10@gmail.com>
2021-04-02 11:09:19 +0200

    Challege 2 : avoid duplicate on random crash

--- challenge/challenge2.py
+++ challenge/challenge2.py
@@ -1,1 +1,25 @@
-## Your solution here.
+import os
+import random
+from uuid import uuid4
+
+from challenge.challenge1 import API1
+
+
+class API2(API1):
+    def create(self, data: dict):
+        """Store one new object.
+        Just adding a check to avoid duplicate entry, so that if program restart
+        can be idempotent in case of random crashes.
+        """
+        self._maybe_crash()
+        new_obj = {**data, "id": uuid4()}
+        if new_obj["id"] not in self._storage:
+            # check if obj already exist or not to avoid duplicate entry
+            self._storage[new_obj["id"]] = new_obj
+        return new_obj
+
+    @staticmethod
+    def _maybe_crash():
+        """Will this crash? No one knows. Very exciting."""
+        if random.random() < 0.01:
+            os._exit(0)
